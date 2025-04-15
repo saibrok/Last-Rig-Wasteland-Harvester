4ecc38e451ed5400f896dddb0229678619d73fa4
Saibrok <litvinsergey@list.ru>
2025-04-16 00:50:44 +0200

    WH-9 [F] настроено переключение сцен и зоны взаимодействия в ровере

--- scripts/rover/rover.gd
+++ scripts/rover/rover.gd
@@ -1,4 +1,12 @@
 extends StaticBody2D
 
+@onready var interaction_hint: Node2D = $InteractionHint
+
 func _ready():
 	add_to_group("rover")
+
+func _physics_process(_delta):
+	if GameManager.combat_arena and GameManager.player:
+		var distance_to_player = global_position.distance_to(GameManager.player.global_position)
+		var can_interact = GameManager.combat_arena.can_interact
+		interaction_hint.visible = can_interact and distance_to_player < 100.0
